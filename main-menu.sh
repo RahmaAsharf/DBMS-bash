9354f325f75da3702d606feda976fb49a36d31ab
DoaaGamal <doaa.gamal02@gmail.com>
2024-03-04 02:31:32 +0200

    Added createDB and list DBs functionality

--- main-menu.sh
+++ main-menu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PS3="Please enter number of your choice: "
+
+main(){
+	PS3="Please enter number of your choice: "
 
 options=("Create Database" "List Database" "Connect to Database" "Drop Database")
 
@@ -9,6 +11,8 @@ select option in "${options[@]}"
 do
 case $REPLY in
 	1) echo "Creating DB"
+       createDB
+	   main
         ;;
 	2) echo "Listing DBs"
 	;;
@@ -20,6 +24,48 @@ case $REPLY in
         ;;
 esac
 done
+}
 
+createDB() {
+    read -p "Please enter Database Name: " dbName
 
+    # check for spaces and special characters
+    if [[ ! $dbName =~ ^[a-zA-Z0-9_]+$ ]]
+	then
+        echo "Error: Database name cannot have space or special characters"
+        echo "------------------------------------"
+        return
+    fi
 
+    # check if DB name starts with a letter
+    if [[ ! $dbName =~ ^[a-zA-Z] ]]
+	then
+        echo "Error: Database name must start with a letter."
+        echo "------------------------------------"
+        return
+    fi
+
+    # check if DB already exists
+    if [ -d "./databases/$dbName" ]
+	then
+        echo "Error: Database $dbName already exists."
+        echo "------------------------------------"
+        return
+    fi
+
+    # create the database dir
+    mkdir -p "./databases/$dbName"
+    if [ $? -eq 0 ]
+	then
+        echo "Database $dbName created successfully!"
+        echo "------------------------------------"
+    else
+        echo "Error creating database $dbName."
+    fi
+}
+
+listDatabases() {
+    ls ./databases
+}
+
+main
